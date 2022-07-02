25a4617bdcd94bb73c60537b20c3d066fc38d31a
praveenreddiey <praveenreddiey@gmail.com>
2022-07-02 17:01:06 +0530

    completed UI elements for team page

--- Ipl Dashboard/src/react-module/src/App.js
+++ Ipl Dashboard/src/react-module/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import { TeamPage } from "./pages/TeamPage";
+import { MatchPage } from "./pages/MatchPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <Routes>
+          <Route path="/teams/:teamName" element={<TeamPage />}></Route>
+          <Route
+            path="/teams/:teamName/matches/:year"
+            element={<MatchPage />}
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- Ipl Dashboard/src/react-module/src/pages/TeamPage.js
+++ Ipl Dashboard/src/react-module/src/pages/TeamPage.js
@@ -1,27 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
 
   useEffect(() => {
     const fetchMatches = async () => {
-      const response = await fetch(
-        "http://localhost:8080/team/Mumbai%20Indians"
-      );
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
       const data = await response.json();
       setTeam(data);
     };
     fetchMatches();
-  }, []);
+  }, [teamName]);
 
+  if (!team || !team.teamName) {
+    return <h1>Team not found</h1>;
+  }
   return (
     <div className="TeampPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]} />
+      <MatchDetailCard match={team.matches[0]} teamName={team.teamName} />
       {team.matches.slice(1).map((match) => (
-        <MatchSmallCard match={match} />
+        <MatchSmallCard match={match} teamName={team.teamName} />
       ))}
     </div>
   );
