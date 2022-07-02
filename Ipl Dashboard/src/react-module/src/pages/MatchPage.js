a59ee8404b1d21075c178dde2ad4678445179e20
praveenreddiey <praveenreddiey@gmail.com>
2022-07-02 18:04:20 +0530

    added match page in react

--- Ipl Dashboard/src/react-module/src/pages/MatchPage.js
+++ Ipl Dashboard/src/react-module/src/pages/MatchPage.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MatchDetailCard } from "../components/MatchDetailCard";
 
 export const MatchPage = () => {
-  return <div>MatchPage</div>;
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(
+        `http://localhost:8080/team/${teamName}/matches?year=${year}`
+      );
+      const data = await response.json();
+      setMatches(data);
+    };
+
+    fetchMatches();
+  }, []);
+  return (
+    <div>
+      <h1>Match Page</h1>
+      {matches.map((match) => (
+        <MatchDetailCard match={match} teamName={teamName} />
+      ))}
+    </div>
+  );
 };
