b7a28057d53d9b78c98581edccee11aec4f96665
praveenreddiey <praveenreddiey@gmail.com>
2022-07-02 17:37:27 +0530

    added match API

--- Ipl Dashboard/src/main/java/com/javabrains/ipldashboard/Repository/MatchRepository.java
+++ Ipl Dashboard/src/main/java/com/javabrains/ipldashboard/Repository/MatchRepository.java
@@ -4,11 +4,21 @@ import com.javabrains.ipldashboard.model.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MatchRepository extends JpaRepository<Match,Long> {
     List<Match> findByTeam1OrTeam2OrderByDateDesc(String team1, String team2, Pageable pageable);
+
+//    List<Match> findByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+//            String team1, LocalDate date1, LocalDate date2,
+//            String team2,LocalDate date3, LocalDate date4);
+
+    @Query("select m from Match m where (m.team1 = :teamName or m.team2= :teamName) and m.date between :dateStart and :dateEnd order by date desc")
+    List<Match> getMatchesByTeamBetweenDates(@Param("teamName") String teamName,@Param("dateStart") LocalDate dateStart,@Param("dateEnd") LocalDate dateEnd);
 
     public default List<Match> findLatestMatchesByTeam(String team1, int count){
         Pageable pageable=PageRequest.of(0, 3);
--- Ipl Dashboard/src/main/java/com/javabrains/ipldashboard/controller/TeamController.java
+++ Ipl Dashboard/src/main/java/com/javabrains/ipldashboard/controller/TeamController.java
@@ -2,9 +2,11 @@ package com.javabrains.ipldashboard.controller;
 
 import com.javabrains.ipldashboard.Repository.MatchRepository;
 import com.javabrains.ipldashboard.Repository.TeamRepository;
+import com.javabrains.ipldashboard.model.Match;
 import com.javabrains.ipldashboard.model.Team;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +26,12 @@ public class TeamController {
         Team team = teamRepository.findByTeamName(teamName).get();
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName,4));
         return team;
+    }
+    @GetMapping("/{teamName}/matches")
+    public List<Match> getMatches(@PathVariable String teamName, @RequestParam int year){
+        LocalDate firstDate=LocalDate.of(year,1,1);
+        LocalDate lastDate=LocalDate.of(year+1,1,1);
+        return matchRepository.getMatchesByTeamBetweenDates(
+                teamName,firstDate,lastDate);
     }
 }
